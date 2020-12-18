4310d5da7473fe2217657adc39e0ce71644379bb
jaguaras999@gmail.com <jaguaras999@gmail.com>
2020-12-18 16:45:30 +0200

    Nauji pakeitimai. Pridejau nauja eilute.

--- Studentai/Studentai.cpp
+++ Studentai/Studentai.cpp
@@ -6,16 +6,9 @@
 int main()
 {
     std::cout << "Hello World!\n";
-    std::cout << "Geras kodas.!\n";
-    std::cout << "Geras kodas.!\n";
-    std::cout << "Geras kodas.!\n";
-    std::cout << "Geras kodas.!\n";
-    std::cout << "Geras kodas.!\n";
-    std::cout << "Geras kodas.!\n";
-    std::cout << "Geras kodas.!\n";
-    std::cout << "Geras kodas.!\n";
-    std::cout << "Geras kodas.!\n";
-    std::cout << "Geras kodas.!\n";
+
+    std::cout << "Hello World!\n";
+    
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
